10d070459d860da016941c99f954bdd11dc1fe8c
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-11-05 14:56:00 +0100

    make clippy happy

--- src/orderbook.rs
+++ src/orderbook.rs
@@ -17,7 +17,7 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 pub extern crate alloc;
 
-use crate::{AccountId, AssetId, Balance, BalancesData};
+use crate::{AccountId, AssetId, Balance};
 use alloc::{vec, vec::Vec};
 use codec::{Decode, Encode, Error};
 use frame_support::sp_runtime::traits::Verify;
