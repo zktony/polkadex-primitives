b2435d50830895f4810db2a1ca1b885a785a01c1
arrudagates <arrudagates@gmail.com>
2021-12-01 16:46:10 -0300

    feat: Implemented Display for AssetId

--- src/assets.rs
+++ src/assets.rs
@@ -20,6 +20,8 @@ use codec::{Decode, Encode};
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
 use sp_core::RuntimeDebug;
+#[cfg(feature = "std")]
+use std::fmt::Display;
 
 /// Enumerated asset on chain
 #[derive(Encode, Decode, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, RuntimeDebug)]
@@ -31,4 +33,18 @@ pub enum AssetId {
     /// Range 0 - 0x00000000FFFFFFFF (2^32)-1 is reserved for protected tokens
     /// the values under 1000 are used for ISO 4217 Numeric Curency codes
     Asset(u64),
+}
+
+#[cfg(feature = "std")]
+impl Display for AssetId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                AssetId::POLKADEX => String::from("POLKADEX"),
+                AssetId::Asset(code) => code.to_string(),
+            }
+        )
+    }
 }
