299103d19c61abaced278f15281689bc726eebdf
Gautham <Gauthamastro@gmail.com>
2022-04-16 10:55:35 +0530

    Add MaxEncodedLen to AssetId

--- src/assets.rs
+++ src/assets.rs
@@ -16,13 +16,13 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use codec::{Decode, Encode};
+use codec::{Decode, Encode, MaxEncodedLen};
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
 use sp_core::{RuntimeDebug};
 
 /// Enumerated asset on chain
-#[derive(Encode, Decode, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, RuntimeDebug)]
+#[derive(Encode, Decode, MaxEncodedLen, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, RuntimeDebug)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub enum AssetId {
     /// PDEX the native currency of the chain
