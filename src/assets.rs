b0f7f158dd7f6a6770ff49971cc7b144a0fe3420
Gautham <Gauthamastro@gmail.com>
2022-04-16 10:59:04 +0530

    Add TypeInfo

--- src/assets.rs
+++ src/assets.rs
@@ -17,12 +17,13 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use codec::{Decode, Encode, MaxEncodedLen};
+use scale_info::TypeInfo;
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
 use sp_core::{RuntimeDebug};
 
 /// Enumerated asset on chain
-#[derive(Encode, Decode, MaxEncodedLen, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, RuntimeDebug)]
+#[derive(Encode, Decode, TypeInfo, MaxEncodedLen, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, RuntimeDebug)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub enum AssetId {
     /// PDEX the native currency of the chain
