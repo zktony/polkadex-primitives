d3b20dd5afb86b469f54278762949f6187d9d59d
Gautham <Gauthamastro@gmail.com>
2022-04-17 13:53:37 +0530

    Update assets.rs

--- src/assets.rs
+++ src/assets.rs
@@ -26,7 +26,7 @@ use sp_core::RuntimeDebug;
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub enum AssetId {
     /// PDEX the native currency of the chain
-    POLKADEX,
+    polkadex,
     /// Generic enumerated assed
     /// Range 0 - 0x00000000FFFFFFFF (2^32)-1 is reserved for protected tokens
     /// the values under 1000 are used for ISO 4217 Numeric Curency codes
