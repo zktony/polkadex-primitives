82f9f0a525ee459fc569427fb161c8fc060c4466
Gautham <gauthamastro@gmail.com>
2022-05-25 14:07:10 +0530

    Fix build issues

--- src/ocex.rs
+++ src/ocex.rs
@@ -57,7 +57,7 @@ pub struct AccountInfo<Account,ProxyLimit: Get<u32>> {
     proxies: BoundedVec<Account,ProxyLimit>
 }
 
-impl<Account,ProxyLimit: Get<u32>> AccountInfo<Account,ProxyLimit> {
+impl<Account: PartialEq,ProxyLimit: Get<u32>> AccountInfo<Account,ProxyLimit> {
     pub fn new(proxy: Account) -> AccountInfo<Account,ProxyLimit>{
         let mut proxies = BoundedVec::default();
         if let Err(()) = proxies.try_push(proxy){
@@ -67,7 +67,7 @@ impl<Account,ProxyLimit: Get<u32>> AccountInfo<Account,ProxyLimit> {
     }
 
     // Adds a new proxy account
-    pub fn add_proxy(&mut self, proxy: &Account) -> Result<(),()> {
+    pub fn add_proxy(&mut self, proxy: Account) -> Result<(),()> {
         self.proxies.try_push(proxy)
     }
 
